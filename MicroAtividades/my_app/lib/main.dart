848863b156910f15276971fd93299b9fa54633e0
MariLFO <mari.fer.onorio@gmail.com>
2024-01-27 23:03:12 -0300

    MicroAtividade 4

--- MicroAtividades/my_app/lib/main.dart
+++ MicroAtividades/my_app/lib/main.dart
@@ -19,21 +19,30 @@ class StatelessWidgetExemplo extends StatelessWidget {
         title: Text(_appBarTitle),
       ),
       body: Center(
-        child: // Exemplo de uma linha (Row) com três colunas
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Column(
-              children: <Widget>[Icon(Icons.call), Text('Call')],
-            ),
-            Column(
-              children: <Widget>[Icon(Icons.directions), Text('Route')],
-            ),
-            Column(
-              children: <Widget>[Icon(Icons.share), Text('Share')],
-            ),
-          ],
-        )
+          child: // Exemplo de uma linha (Row) com três colunas
+          // Exemplo de uma ListView com três ListTile
+          ListView(
+            children: const [
+              ListTile(
+                title: Text('Flutter'),
+                subtitle: Text('Tudo é um widget'),
+                leading: Icon(Icons.flash_on),
+                trailing: Icon(Icons.keyboard_arrow_right),
+              ),
+              ListTile(
+                title: Text('Dart'),
+                subtitle: Text('É fácil'),
+                leading: Icon(Icons.mood),
+                trailing: Icon(Icons.keyboard_arrow_right),
+              ),
+              ListTile(
+                title: Text('Firebase'),
+                subtitle: Text('Combina com Flutter'),
+                leading: Icon(Icons.whatshot),
+                trailing: Icon(Icons.keyboard_arrow_right),
+              ),
+            ],
+          )
       ),
     );
   }
