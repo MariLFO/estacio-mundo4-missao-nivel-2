8faa90f373ae305597e1372525f31ed166d54b08
MariLFO <mari.fer.onorio@gmail.com>
2024-01-27 23:44:28 -0300

    MissaoPratica - Sessão de Texto

--- MissaoPratica/explore_mundo/lib/main.dart
+++ MissaoPratica/explore_mundo/lib/main.dart
@@ -74,6 +74,14 @@ class MyApp extends StatelessWidget {
       ],
     );
 
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+        'O Lago Oeschinen fica aos pés do Blüemlisalp nos Alpes Berneses. Situado a 1.578 metros acima do nível do mar, é um dos lagos alpinos mais amplos. Um passeio de teleférico a partir de Kandersteg, seguido por meia hora de caminhada por pastagens e floresta de pinheiros, leva você ao lago, que aquece até 20 graus Celsius no verão. As atividades desfrutadas aqui incluem remo e andar no tobogã de verão.',
+        softWrap: true,
+      ),
+    );
+
     return MaterialApp(
       title: 'Demonstração de layout Flutter',
       home: Scaffold(
@@ -84,6 +92,7 @@ class MyApp extends StatelessWidget {
           children: [
             titleSection,
             buttonSection,
+            textSection,
             // Outros elementos do corpo do aplicativo podem ser adicionados aqui.
           ],
         ),
